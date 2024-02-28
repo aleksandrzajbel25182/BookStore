48ed04f675873f1da1904d87eb4b398508dc8817
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-02-28 10:11:24 +0700

    Добавил контроллер

---
+++ BookStore.Api/Controllers/BookResponce.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Api.Controllers
+{
+    public record BookResponce(
+        Guid id,
+        string Title,
+        string Description,
+        decimal Price);
+}
---
+++ BookStore.Api/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Core.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksServices _bookService;
+
+        public BooksController(IBooksServices booksService)
+        {
+            _bookService = booksService;    
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<BookResponce>>> GetBooks()
+        {
+            var books = await _bookService.GetAllBooks();
+
+            var responce = books.Select(b => new BookResponce(b.Id, b.Title, b.Description, b.Price));
+            
+            
+            return Ok(responce);
+
+        }
+
+       
+    }
+}
