c7d437fd6fc7406476392886a7163bd081975038
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-02-29 14:55:23 +0700

    Что-то непонятное сделал

--- frontend/bookstore/app/books/page.tsx
+++ frontend/bookstore/app/books/page.tsx
@@ -2,18 +2,27 @@
 
 import Button from "antd/es/button/button"
 import { Books } from "../components/Books";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getAllBooks } from "../services/books";
 
-export default function BookPage() {
+export default function BooksPage() {
+    const [books, setBooks] = useState<Book[]>([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-
-    }, [])
+        const getBooks = async () => {
+            const books = await getAllBooks();
+            setLoading(false);
+            setBooks(books);
+        }
+        getBooks();
+    }, []);
 
     return (
         <div>
             <Button>Add book</Button>
 
-            <Books book={books} />
+            <Books books={books} />
         </div>
     )
-} ы
+}
